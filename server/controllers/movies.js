546e1dd2561da7479f68df81b0fea2d2b210ad66
sam <samuel1haim@gmail.com>
2021-06-29 10:10:06 +0100

    i have added try-catch block while calling the server on moviesController.get_all

--- server/controllers/movies.js
+++ server/controllers/movies.js
@@ -2,6 +2,11 @@ const {Movie} = require('../models/movie');
 const mongoose = require('mongoose');
 
 exports.get_all = async(req, res) => {
-	const movies = await Movie.find().sort('title');
-	res.send(movies);
+	try {
+		const movies = await Movie.find().sort('title');
+		res.send(movies);
+	} catch (ex) {
+		console.log('Oh no. We have an error...');
+		res.status(500).send('Oh no. We have an error...');
+	}
 };

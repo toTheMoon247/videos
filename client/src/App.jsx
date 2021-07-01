d1ff73d9d5722472856d6921c26927f819d6cd65
sam <samuel1haim@gmail.com>
2021-07-01 16:09:46 +0100

    i have added navbar that redirect to movies and customers

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,21 @@
-import { Route } from 'react-router-dom';
+import React from 'react';
+import { Route, Redirect, Switch } from 'react-router-dom';
 import Movies from './components/movies';
 import Customers from './components/customers';
+import NavBar from './components/navBar';
 import './App.css';
 
 function App() {
   return (
-    <main className="container">
-       <Route path="/movies" component={Movies}></Route>
-       <Route path="/customers" component={Customers}></Route>
-     </main>
+    <React.Fragment>
+      <NavBar />
+      <main className="container">
+        <Switch>
+          <Route path="/movies" component={Movies}></Route>
+          <Route path="/customers" component={Customers}></Route>
+        </Switch>
+       </main>
+    </React.Fragment>
   );
 }
 
